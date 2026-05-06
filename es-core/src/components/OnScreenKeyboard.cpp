bce8a3b0dd37a8d06dca2f065e07fbd709c7876f
2play <2playbuilds@gmail.com>
2026-05-06 14:09:57 +0200

    Update OnScreenKeyboard.cpp

--- es-core/src/components/OnScreenKeyboard.cpp
+++ es-core/src/components/OnScreenKeyboard.cpp
@@ -1,22 +1,93 @@
-// OnScreenKeyboard.cpp
 #include "OnScreenKeyboard.h"
 #include "Renderer.h"
+#include "resources/Font.h"
 
-OnScreenKeyboard::OnScreenKeyboard(Window* window) : GuiComponent(window) {
-    cursorX = cursorY = 0;
+OnScreenKeyboard::OnScreenKeyboard(Window* window)
+    : GuiComponent(window), cursorX(0), cursorY(0), currentPage(PAGE_LETTERS)
+{
+    loadPage(PAGE_LETTERS);
 }
 
-void OnScreenKeyboard::render(const Transform4x4f& parentTrans) {
-    // Draw a simple grid of letters
-    // Highlight cursor position
+void OnScreenKeyboard::loadPage(KeyboardPage page)
+{
+    currentPage = page;
+    mKeys.clear();
+
+    if (page == PAGE_LETTERS) {
+        mKeys = {
+            "A","B","C","D","E","F","G","H","I",
+            "J","K","L","M","N","O","P","Q","R",
+            "S","T","U","V","W","X","Y","Z"," ",
+            "<","123","!@#"
+        };
+    } else if (page == PAGE_NUMBERS) {
+        mKeys = {
+            "0","1","2","3","4","5","6","7","8","9",
+            "-","_","+","=","/",":",";",".",",","?",
+            "<","ABC","!@#"
+        };
+    } else if (page == PAGE_SYMBOLS) {
+        mKeys = {
+            "!","@","#","$","%","^","&","*","(",")",
+            "[","]","{","}","<",">","|","\\","'","\"",
+            "<","ABC","123"
+        };
+    }
 }
 
-void OnScreenKeyboard::onInput(const InputCompactEvent& event) {
-    // Handle d-pad navigation
-    // Handle A button to select letter
-    // Append to mText
+void OnScreenKeyboard::render(const Transform4x4f& parentTrans)
+{
+    Transform4x4f trans = parentTrans;
+    Renderer::setMatrix(trans);
+
+    auto font = Font::get(FONT_SIZE_MEDIUM);
+    int cols = 9;
+    int cellW = 40;
+    int cellH = 40;
+
+    for (size_t i = 0; i < mKeys.size(); i++) {
+        int x = (i % cols) * cellW;
+        int y = (i / cols) * cellH;
+
+        unsigned int color = 0xFFFFFFFF;
+        if ((int)(i % cols) == cursorX && (int)(i / cols) == cursorY)
+            color = 0x00FF00FF; // highlight cursor
+
+        font->drawText(mKeys[i], x, y, color);
+    }
 }
 
-std::string OnScreenKeyboard::getText() const {
+void OnScreenKeyboard::onInput(const InputCompactEvent& event)
+{
+    int cols = 9;
+    int rows = (mKeys.size() + cols - 1) / cols;
+
+    if (event.down) {
+        if (event.key == SDLK_LEFT) cursorX = (cursorX + cols - 1) % cols;
+        if (event.key == SDLK_RIGHT) cursorX = (cursorX + 1) % cols;
+        if (event.key == SDLK_UP) cursorY = (cursorY + rows - 1) % rows;
+        if (event.key == SDLK_DOWN) cursorY = (cursorY + 1) % rows;
+        if (event.key == SDLK_RETURN) {
+            int idx = cursorY * cols + cursorX;
+            if (idx < (int)mKeys.size()) {
+                std::string key = mKeys[idx];
+                if (key == "<") {
+                    if (!mText.empty()) mText.pop_back();
+                } else if (key == "ABC") {
+                    loadPage(PAGE_LETTERS);
+                } else if (key == "123") {
+                    loadPage(PAGE_NUMBERS);
+                } else if (key == "!@#") {
+                    loadPage(PAGE_SYMBOLS);
+                } else {
+                    mText += key;
+                }
+            }
+        }
+    }
+}
+
+std::string OnScreenKeyboard::getText() const
+{
     return mText;
 }
