78be0f29aa38598f5e390dae6e28f88ad977fa7f
2play <2playbuilds@gmail.com>
2026-05-06 14:09:34 +0200

    Update OnScreenKeyboard.h

--- es-core/src/components/OnScreenKeyboard.h
+++ es-core/src/components/OnScreenKeyboard.h
@@ -1,14 +1,23 @@
-// OnScreenKeyboard.h
 #pragma once
 #include "GuiComponent.h"
+#include <vector>
+#include <string>
+
+enum KeyboardPage { PAGE_LETTERS, PAGE_NUMBERS, PAGE_SYMBOLS };
 
 class OnScreenKeyboard : public GuiComponent {
 public:
     OnScreenKeyboard(Window* window);
+
     void render(const Transform4x4f& parentTrans) override;
     void onInput(const InputCompactEvent& event) override;
     std::string getText() const;
+
 private:
+    void loadPage(KeyboardPage page);
+
+    std::vector<std::string> mKeys;
     std::string mText;
     int cursorX, cursorY;
+    KeyboardPage currentPage;
 };
